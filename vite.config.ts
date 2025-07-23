eda80dc1cba62e89754a3544d050ecf630397ffb
Victoria <hvangulo@espol.edu.ec>
2025-07-23 18:48:47 -0500

    index y vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Dashboard/', // Asegúrate que coincida con tu repositorio
+  base: "./",
   plugins: [react()],
   build: {
     outDir: 'dist',
