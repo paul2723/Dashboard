9b69b72c61f289df3795637aa14a56cabf02b191
paul123 <139300207+paul2723@users.noreply.github.com>
2025-07-23 13:13:52 +0000

    cambios pequeños

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: './', // Para desarrollo local usa './'
+  base: './',
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true
-  }
-})
+});
