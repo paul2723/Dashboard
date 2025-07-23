debabcda77ba49abf360ab357ef177da5c20ed39
paul123 <139300207+paul2723@users.noreply.github.com>
2025-07-23 13:01:08 +0000

    cambios

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/sky-view-reports/', // Debe coincidir con el nombre de tu repositorio
+  base: './', // Para desarrollo local usa './'
+  plugins: [react()],
   build: {
     outDir: 'dist',
-    emptyOutDir: true,
-  },
-  plugins: [react()]
-});
+    emptyOutDir: true
+  }
+})
