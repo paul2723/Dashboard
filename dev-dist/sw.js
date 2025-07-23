8a9b7e2659992221d5722b5ac2a2979f2e8d6ec4
paul123 <139300207+paul2723@users.noreply.github.com>
2025-07-23 06:43:17 +0000

    crear carpeta

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-e755d862'], (function (workbox) { 'use strict';
     "revision": "21199adf659a9b979572e74b6e36502f"
   }, {
     "url": "index.html",
-    "revision": "0.jes7vjgmef8"
+    "revision": "0.dquiae6aof8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
