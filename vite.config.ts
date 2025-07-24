f7784b90430a863c5e41bc069dee4e897609038e
paul123 <139300207+paul2723@users.noreply.github.com>
2025-07-24 00:00:09 +0000

    dashboard

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react' // o tu framework
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // Cambia de '/' a './' para rutas relativas
+  base: './Dashboard/', // Cambia de '/' a './' para rutas relativas
   build: {
     outDir: 'dist'
   }
