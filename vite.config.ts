f71891705e8c5727669fa38bb2747aa72350e306
Hilda Victoria Angulo Borja <134457751+vic28code@users.noreply.github.com>
2025-07-24 00:38:43 +0000

    sky view reports

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // Cambia de '/' a './' para rutas relativas
-  build: {
-    outDir: 'dist'
-  }
+  base: '/sky-view-reports/',
 })
