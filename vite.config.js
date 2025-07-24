ca5f00bbdc0f382896a93a6dee03773160bb55e0
paul123 <139300207+paul2723@users.noreply.github.com>
2025-07-24 03:37:15 +0000

    destruccion

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import fs from 'fs' // Añade esta importación
-import path from 'path' // Añade esta importación
+import { resolve } from 'path'
+import fs from 'fs'
 
 export default defineConfig({
   plugins: [
@@ -9,9 +9,13 @@ export default defineConfig({
     {
       name: 'add-nojekyll',
       closeBundle() {
-        const nojekyllPath = path.join(__dirname, 'dist', '.nojekyll')
-        if (!fs.existsSync(nojekyllPath)) {
-          fs.writeFileSync(nojekyllPath, '')
+        const nojekyllPath = resolve(__dirname, 'dist', '.nojekyll')
+        fs.writeFileSync(nojekyllPath, '')
+        
+        // Añadir CNAME si usas dominio personalizado
+        const cnamePath = resolve(__dirname, 'dist', 'CNAME')
+        if(fs.existsSync('CNAME') && !fs.existsSync(cnamePath)) {
+          fs.copyFileSync('CNAME', cnamePath)
         }
       }
     }
@@ -19,6 +23,7 @@ export default defineConfig({
   base: './',
   build: {
     outDir: 'dist',
+    emptyOutDir: true,
     assetsDir: 'assets',
     rollupOptions: {
       output: {
