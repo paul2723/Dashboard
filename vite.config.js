fc1a1ff27a7a9f02caec771dae29fa79b85369f9
paul123 <139300207+paul2723@users.noreply.github.com>
2025-07-24 00:41:15 +0000

    cambios

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/sky-view-reports/', // Cambiar de /Dashboard/ a /sky-view-reports/
+  base: '/Dashboard/', // Cambiar de /sky-view-reports/ a /Dashboard/
   build: {
     outDir: 'dist',
     assetsDir: 'assets'
